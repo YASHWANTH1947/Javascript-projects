4919c177656e5679548634922b037c67f3eda6d7
Yashwanth <yashwanthreddy1947@gmail.com>
2024-11-18 09:49:54 +0530

    timer complete

--- count_down_timer/apps.js
+++ count_down_timer/apps.js
@@ -1,21 +1,26 @@
 let newyear='1 jan 2025';
 let d1=new Date(newyear)
 console.log(d1);
-let monthT=document.querySelector('#months');
-let daysT=document.querySelector('#days');
-let hoursT=document.querySelector('#hours');
-let minT=document.querySelector('#min');
-let secT=document.querySelector('#sec')
+let monthT=document.querySelector('#months>span');
+let daysT=document.querySelector('#days>span');
+let hoursT=document.querySelector('#hours>span');
+let minT=document.querySelector('#min>span');
+let secT=document.querySelector('#sec>span')
 function  timer(){
     let d2=new Date()
     let diff=d1-d2;
     const totalSeconds = (d1 - d2) / 1000;
-
+const months=Math.floor((totalSeconds % 31536000) / 2628000);
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
     console.log(days,hours,mins,seconds);
+    daysT.innerHTML=days;
+    hoursT.innerHTML=hours;
+    minT.innerHTML=mins;
+    monthT.innerHTML=months;
+    secT.innerHTML=seconds;
 }
 
 setInterval(timer,1000)
